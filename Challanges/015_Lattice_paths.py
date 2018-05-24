314827f9dfdff7c1a99346db94674d122e2629a8
Mo0dy - Felix Mühlenberend <felix.muehlenberend@gmail.com>
2018-05-24 12:33:05 +0200

    lattice paths

---
+++ Challanges/015_Lattice_paths.py
@@ -0,0 +1,18 @@
+# https://projecteuler.net/problem=15
+
+'''
+Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+
+How many such routes are there through a 20×20 grid?
+'''
+
+
+rows = 2
+columns = 2
+
+# start from the upper right position (0, 0)
+for r in range(rows):
+    for c in range(columns):
+
+
+
